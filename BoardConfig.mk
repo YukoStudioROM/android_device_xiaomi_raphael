2b87305ea11b5fc2f5607461b3c4fe626b9afa10
Rasenkai <rasenkai99@gmail.com>
2021-08-07 18:43:22 +0200

    raphael: Disable NFC for indian devices
    * Setup raphael sku when device is global or chinese.
    * Move the nfc manifest entry to raphael hardware sku
      variant.
    
    [poad42]
    * drop the init file part, we'll use vendor init to set sku in a later commit
    
    Change-Id: If6b472508f43c87c772f2d6b3d4d4a45faacfc30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,10 @@ TARGET_USES_FOD_ZPOS := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+ODM_MANIFEST_SKUS += raphael
+ODM_MANIFEST_RAPHAEL_FILES := \
+    $(DEVICE_PATH)/manifest-nfc.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
 TARGET_RECOVERY_DEVICE_MODULES := libinit_raphael
